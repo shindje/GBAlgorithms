90181b0c1e34d1abf70162ee5c599c87ff068860
aleks <aleks.shindje@gmail.com>
2020-06-30 01:32:48 +0300

    Homework2

--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -8,8 +8,22 @@ public class Main {
         arr = new int[5];
         int[] arr2 = {1, 2, 3, 4};
 
-        Array a0 = new Array(1, -2, -3, -4, 5);
-        a0.sortBubble();
-        System.out.println(a0);
+        Array a = new Array(1, -2, 3, 5, -4, 7, -2, 67, -3, -76);
+        System.out.println("O(n^2): " + a.length() * a.length());
+        System.out.println("sortBubble. Операций: " + a.copyOf().sortBubble());
+        System.out.println("sortBubbleBetter. Операций: " + a.copyOf().sortBubbleBetter());
+        System.out.println("sortInsert. Операций: " + a.copyOf().sortInsert());
+        System.out.println("sortSelect. Операций: " + a.copyOf().sortSelect());
+        Array a1 = a.copyOf();
+        System.out.println("sortCount. Операций: " + a1.sortCount());
+        System.out.println(a1);
+        a.insert(3, 999);
+        System.out.println(a);
+        a.deleteValue(-4);
+        System.out.println(a);
+        a.delete(2);
+        System.out.println(a);
+        a.deleteAll();
+        System.out.println(a);
     }
 }
